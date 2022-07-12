0d79970e87d3a24ae08296b4ae3880decccebe8b
Baezor <64096464+Baezor1@users.noreply.github.com>
2022-07-12 16:43:44 -0500

    Create Projects.svelte

---
+++ src/components/Projects.svelte
@@ -0,0 +1,29 @@
+<script>
+    import Card from "./ProjectCard.svelte"
+    import Title from "./Title.svelte"
+</script>
+
+<div class="projects-container">
+    <Card
+        title="Skript"
+        desc="Skript is a language used to create Minecraft servers. I've been using Skript for over 3 years and it got me into programming."
+        img="https://avatars.githubusercontent.com/u/39464898?s=200&v=4"
+        link="/projects/skript"
+    />
+</div>
+
+<style>
+    .projects-container {
+        width: 100vw;
+        margin: auto;
+        padding-bottom: 5vh;
+        display: flex;
+        justify-content: center;
+        align-items: left;
+        align-content: center;
+        flex-flow: row wrap;
+        border-bottom: 1px solid rgba(255, 255, 255, .2);
+        padding-top: 5vh;
+    }
+
+</style>
