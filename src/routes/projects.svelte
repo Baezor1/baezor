a0992aa4e06e92c3afb0b939ffd68143239a2824
Baezor <64096464+Baezor1@users.noreply.github.com>
2022-07-12 16:43:48 -0500

    Create projects.svelte

---
+++ src/routes/projects.svelte
@@ -0,0 +1,51 @@
+<script>
+    import NavBar from "../components/NavBar.svelte"
+    import Footer from "../components/Footer.svelte"
+    import Projects from "../components/Projects.svelte"
+    import About from "../components/About.svelte"
+</script>
+
+<div class="header">
+    <div class="container">
+        <h1>Projects</h1>
+    </div>
+</div>
+<Projects />
+<Footer />
+<NavBar />
+
+<style>
+    .header {
+        width: 100vw;
+        text-align: center;
+        padding-top: 10vh;
+        padding-bottom: 10vh;
+        background: rgb(18,18,18);
+        background: linear-gradient(128deg, rgba(18,18,18,1) 0%, rgba(22,42,63,1) 28%, rgba(177,34,71,1) 100%);
+        border-bottom: 1px solid rgba(255, 255, 255, .2);
+        margin-bottom: 0;
+    }
+
+    .container {
+        width: 40vw;
+        margin: auto;
+        padding-top: 0;
+    }
+
+    .container h1 {
+        font-size: 5rem;
+        margin-bottom: 0;
+    }
+
+    @media (max-width: 768px) {
+
+        .container h1 {
+            font-size: 4rem;
+        }
+
+        .container {
+            width: 80vw;
+        }
+    }
+
+</style>
