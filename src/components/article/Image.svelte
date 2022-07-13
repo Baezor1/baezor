3c76baebc2e1fc704cd36114637ab5ccdbea3293
Baezor <64096464+Baezor1@users.noreply.github.com>
2022-07-13 17:35:13 -0500

    Create Image.svelte

---
+++ src/components/article/Image.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let img
+</script>
+
+<div class="img-container">
+    <img src={img} alt="">
+</div>
+
+<style>
+    .img-container {
+        width: 100vw;
+    }
+
+    .img-container img {
+        max-width: 50vw;
+        margin-left: 25vw;
+        margin-top: 5vh;
+        margin-bottom: 5vh;
+    }
+</style>
