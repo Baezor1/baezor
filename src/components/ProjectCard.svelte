27ed4da2974b2826524c4425f12b19ee85d5382e
Baezor <64096464+Baezor1@users.noreply.github.com>
2022-07-27 13:00:03 -0500

    Update ProjectCard.svelte

--- src/components/ProjectCard.svelte
+++ src/components/ProjectCard.svelte
@@ -1,80 +1,102 @@
 <script>
     export let title
     export let desc
-    export let tag
+    export let year
     export let link
-
-    if (!tag) {
-        tag = "Other"
-    }
+    export let github
+    export let discord
 </script>
 
 <div class="card">
-    <a href={link} on:click={() => {window.location.href = link}}>
+    <div class="card-container card-container-small">
         <h1>{title}</h1>
-        <p>{desc}</p>
-        <div class="tag {tag}">
-            <p>{tag}</p>
+        <h2 style="color: gray;">{year}</h2>
+        <div class="links">
+            {#if github}
+                <a href={github}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                        <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+                    </svg>
+                </a>
+            {/if}
+            {#if discord}
+                <a href={discord}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-discord" viewBox="0 0 16 16">
+                        <path d="M13.545 2.907a13.227 13.227 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.19 12.19 0 0 0-3.658 0 8.258 8.258 0 0 0-.412-.833.051.051 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.041.041 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032c.001.014.01.028.021.037a13.276 13.276 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019c.308-.42.582-.863.818-1.329a.05.05 0 0 0-.01-.059.051.051 0 0 0-.018-.011 8.875 8.875 0 0 1-1.248-.595.05.05 0 0 1-.02-.066.051.051 0 0 1 .015-.019c.084-.063.168-.129.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.052.052 0 0 1 .053.007c.08.066.164.132.248.195a.051.051 0 0 1-.004.085 8.254 8.254 0 0 1-1.249.594.05.05 0 0 0-.03.03.052.052 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.235 13.235 0 0 0 4.001-2.02.049.049 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.034.034 0 0 0-.02-.019Zm-8.198 7.307c-.789 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612Zm5.316 0c-.788 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612Z"/>
+                    </svg>
+                </a>
+            {/if}
+            {#if link}
+                <a href={link}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/>
+                        <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/>
+                    </svg>
+                </a>
+            {/if}
         </div>
-    </a>
+    </div>
+    <div class="card-container">
+        <p>{desc}</p>
+    </div>
 </div>
 
 <style>
 
     .card {
-        width: 17vw;
+        width: 60vw;
+
         background-color: #181818;
         margin: 1vw;
         padding: 1vw;
+        padding-left: 0;
+        padding-right: 0;
         border-radius: 8px;
         text-align: center;
         transition: all 0.5s;
         overflow: hidden;
+        display: flex;
+        justify-content: center;
+        align-items: left;
+        align-content: center;
+        flex-flow: row wrap;
     }
     
+    .card-container {
+        width: 60%;
+        text-align: left;
+        padding: 5%;
+        padding-top: 0;
+        padding-bottom: 0;
+    }
+
+    .card-container-small {
+        width: 20%;
+    }
+
+    .card-container h1 {
+        font-size: 2rem;
+    }
+
+    .card-container h2 {
+        font-size: 1.5rem;
+    }
+
+    .card-container p {
+        font-size: 1.3rem;
+    }
+
     .card:hover {
         transition: all 0.5s;
         transform: scale(1.05);
     }
 
-    .card a {
-        text-decoration: none;
-        color: white;
+    .links svg {
+        margin-right: 1vw;
     }
 
-    .tag {
-        background-color: #85FFBD;
-        background-image: linear-gradient(45deg, #85FFBD 0%, #FFFB7D 100%);
-        color: #121212;
-        border-radius: 8px;
-        width: 70%;
-        margin: auto;
-    }
-
-    .Website {
-        background-color: #FBAB7E;
-        background-image: linear-gradient(62deg, #FBAB7E 0%, #F7CE68 100%);
-    }
-    .Other {
-        background-color: #FF9A8B;
-        background-image: linear-gradient(90deg, #FF9A8B 0%, #FF6A88 55%, #FF99AC 100%);
-    }
-
-    .Skript {
-        background-color: #08AEEA;
-        background-image: linear-gradient(0deg, #08AEEA 0%, #2AF598 100%);
-
-    }
-
-    .Experiment {
-        background-color: #FEE140;
-        background-image: linear-gradient(90deg, #FEE140 0%, #FA709A 100%);
-
-    }
-
-    .tag p {
-        padding: 5px;
-        font-weight: 500;
+    .links {
+        padding-bottom: 1vh;        
     }
 
     @media (max-width: 768px) {
@@ -82,12 +104,23 @@
             width: 80vw;
             margin: 3vh;
             padding: 3vh;
+            flex-flow: column wrap;
         }
 
-        .card img {
-            max-width: 20vw;
-            max-height: 20vw;
+        .card-container {
+            width: 100%;
+            text-align: center;
         }
+
+        .card-container-small {
+            width: 100%;
+        }
+
+        .links svg {
+            margin-right: 2.5vw;
+            margin-left: 2.5vw;
+        }
+
     }
 
 </style>
